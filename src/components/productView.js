ffb4cb44057e8f4e7b3a2d854c2db4779e235315
Ozgur GUL <lugrugzo@gmail.com>
2018-10-14 10:43:12 +0300

    ProductView enhanced

--- src/components/productView.js
+++ src/components/productView.js
@@ -1,48 +1,56 @@
-import React, {PureComponent} from 'react';
-import { Text, View, TouchableOpacity, ViewPropTypes, Image } from "react-native";
+import React, { PureComponent } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ViewPropTypes,
+  Image
+} from "react-native";
 import { connect } from "react-redux";
 
 import { addProductToBasket } from "../actions/basket";
 
-import basketicon from '../../assets/basketicon.png';
+import basketicon from "../../assets/basketicon.png";
 
 class ProductView extends PureComponent {
-  render(){
+  render() {
     const { product, onClick, containerStyle, addProductToBasket } = this.props;
     return (
-      <TouchableOpacity onPress={onClick} >
-        <View style={containerStyle} >
-          <Text>{product.title}</Text>
+      <TouchableOpacity onPress={onClick}>
+        <View style={containerStyle}>
+          <Text style={{ flex: 1, fontSize: 17 }}>{product.Name}</Text>
           <TouchableOpacity onPress={() => addProductToBasket(product.title)}>
-            <Image style={{ width: 30, height: 30}} source={basketicon} />
+            <Image style={{ width: 30, height: 30 }} source={basketicon} />
           </TouchableOpacity>
         </View>
       </TouchableOpacity>
-    )
+    );
   }
-} 
+}
 
 ProductView.propTypes = {
-  containerStyle: ViewPropTypes.style,
+  containerStyle: ViewPropTypes.style
 };
 
 ProductView.defaultProps = {
   containerStyle: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-    borderRadius: 5,
+    alignItems: "center",
     margin: 2,
-    padding: 5,
-    minHeight: 30,
+    padding: 10,
     backgroundColor: "white"
   }
 };
 
-const mapStateToProps = state => ({
-});
+const mapStateToProps = state => ({});
 
 const mapDispatchToProps = {
   addProductToBasket
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProductView);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductView);
