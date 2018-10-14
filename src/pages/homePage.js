bb5a81c9eafcc75c3b69365f464446ad314f6d90
Ozgur GUL <lugrugzo@gmail.com>
2018-10-14 10:43:04 +0300

    AsyncStorage removed

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -6,15 +6,10 @@ import {
   TouchableOpacity,
   Image,
   Button,
-  AsyncStorage
 } from "react-native";
-
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { SafeAreaView } from "react-navigation";
-import { BarCodeScanner, Permissions } from 'expo';
-
-import { getProductsFromBasket, setSavedBasketToStore, clean } from "../actions/basket" 
-
+import { BarCodeScanner, Permissions } from "expo";
 import Basket from "../components/basket";
 
 class Home extends React.Component {
@@ -24,33 +19,29 @@ class Home extends React.Component {
       headerRight: (
         <TouchableOpacity
           underlayColor="#fff"
-          onPress={() => navigation.navigate('ProductListPage')}
+          onPress={() => navigation.navigate("ProductListPage")}
         >
-          <Image source={require("../../assets/searchicon.png")} style={{width: 30, height: 30}} />
+          <Image
+            source={require("../../assets/searchicon.png")}
+            style={{ width: 30, height: 30 }}
+          />
         </TouchableOpacity>
       )
     };
   };
 
   state = {
-    hasCameraPermission: null,
-  }
+    hasCameraPermission: null
+  };
 
   async componentWillMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({hasCameraPermission: status === 'granted'});
-
-    //this.props.clean();
-
-    AsyncStorage.getItem("@basket")
-    .then((data) => {
-      this.props.setSavedBasketToStore(data == null ? [] : data);
-    })
+    this.setState({ hasCameraPermission: status === "granted" });
   }
 
   onPressExploreButton = () => {
     this.props.navigation.navigate("ProductListPage");
-  }
+  };
 
   render() {
     const { hasCameraPermission } = this.state;
@@ -64,18 +55,15 @@ class Home extends React.Component {
     }
 
     return (
-      <SafeAreaView style={{flex: 1}}>
-        <View style={{ flex: 1, justifyContent:"space-between" }}>
-          <View style={{flex: 1}}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: "space-between" }}>
+          <View style={{ flex: 1 }}>
             <BarCodeScanner
               onBarCodeScanned={this.handleBarCodeScanned}
               style={StyleSheet.absoluteFill}
             />
           </View>
-          <Button
-            title="Ürünlere Göz At"
-            onPress={this.onPressExploreButton}
-          />
+          <Button title="Ürünlere Göz At" onPress={this.onPressExploreButton} />
 
           <Basket />
         </View>
@@ -85,16 +73,15 @@ class Home extends React.Component {
 
   handleBarCodeScanned = ({ type, data }) => {
     this.props.navigation.navigate("ProductDetailPage", { barcode: data });
-  }
+  };
 }
 
-const mapStateToProps = state => ({
-});
+const mapStateToProps = state => ({});
 
 const mapDispatchToProps = {
-  getProductsFromBasket,
-  setSavedBasketToStore,
-  clean
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
--- src/reducers/basket.js
+++ src/reducers/basket.js
@@ -1,11 +1,5 @@
-import { Map } from 'immutable';
-
-import { AsyncStorage } from "react-native";
-
-import {
-  SET_SAVED_BASKET_TO_STORE
-  ,ADD_PRODUCT_TO_BASKET
-} from '../actions/basket';
+import { Map } from "immutable";
+import { ADD_PRODUCT_TO_BASKET } from "../actions/basket";
 
 const initialState = Map({
   loading: false,
@@ -19,15 +13,7 @@ const basket = (state = initialState, action) => {
       var data = state.get("basket");
       data = data == null ? [] : data.slice();
       data.push(action.data);
-      AsyncStorage.setItem("@basket", JSON.stringify(data));
-
-      return state
-        .set("basket", data)
-
-    case SET_SAVED_BASKET_TO_STORE:
-      return state
-        .set("basket", action.data);
-
+      return state.set("basket", data);
     default:
       return state;
   }
